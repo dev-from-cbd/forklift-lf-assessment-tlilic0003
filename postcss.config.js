3332af3696670262e378e0ec9fc4948f1e393a46
Oleg Gribanov <veryfatwombat@gmail.com>
2025-10-01 06:55:56 +0930

    docs(postcss): add comments to explain configuration options

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+// Export default PostCSS configuration object for CSS processing pipeline
 export default {
+  // Define PostCSS plugins to be used during CSS compilation
   plugins: {
+    // Tailwind CSS plugin for utility-first CSS framework processing
     tailwindcss: {},
+    // Autoprefixer plugin for automatic vendor prefix addition to CSS properties
     autoprefixer: {},
   },
 };
